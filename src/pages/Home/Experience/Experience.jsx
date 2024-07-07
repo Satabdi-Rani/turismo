4565426dfa0d7c48cda72271362aea56d81d8453
Satabdi <satabdiraniweb@gmail.com>
2024-07-07 12:12:09 +0600

    Experience section added

--- src/pages/Home/Experience/Experience.jsx
+++ src/pages/Home/Experience/Experience.jsx
@@ -1,12 +1,14 @@
 
 const Experience = () => {
     return (
-        <div className="bg-[#1288B8] pt-32">
+        <div className="bg-[#1288B8] pt-32 pb-32">
             <div className="container mx-auto">
-                <h4 className=" text-xl mb-2 uppercase ">travel blog</h4>
-                <h1 className="text-5xl font-bold uppercase"><span className="">travel</span> <span className="">experience</span></h1>
-                <div className="grid grid-cols-1 sm:grid-cols-3">
-                    
+                <h4 className=" text-xl mb-2 uppercase text-white">travel blog</h4>
+                <h1 className="text-5xl font-bold uppercase text-white"><span className="">travel</span> <span className="">experience</span></h1>
+                <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 mt-10">
+                    <img className="h-[600px] w-[400px] rounded-lg " src="https://i.ibb.co/99GthkV/happy-hipster-young-woman-carry-backpack-travel-tourist-woman-with-backpack-outdoors-holidays-women.jpg" alt="" />
+                    <img className="h-[600px] w-[400px] rounded-lg " src="https://i.ibb.co/FXJH3j5/man-leaning-railing-listening-music.jpg" alt="" />
+                    <img className="h-[600px] w-[400px] rounded-lg " src="https://i.ibb.co/YRS9TJy/stylish-brunette-standing-entance-building.jpg" alt="" />
                 </div>
             </div>
         </div>
